66c7b6306f6ebc1f04c5955f71b4fda2208dd112
kephin <wunshon76325@gmail.com>
2017-12-11 23:51:04 +0800

    update input value by state object

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
+// 'State' is a plain object to record and react to user event
+// each class-based component that we defined has its own state object 
+// whenever a component state is changed, the component immediately re-renders and also forces all of its children to re-renders as well
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    // only set a object inside constructor, in other places we only use this.setState({...}) to update our state object
+    this.state = { term: ''};
+  }
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        <p>Value of the input is: {this.state.term}</p>
+      </div>
+    )
+
   }
 }
 
