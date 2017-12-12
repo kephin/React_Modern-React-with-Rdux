9599339608350ba117d0d050455f3f051bb40371
kephin <wunshon76325@gmail.com>
2017-12-12 22:31:42 +0800

    refactor App to class component due to the need to keep track on youtube API response

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBRLw_tEu9-hz8hNlaH9K_Do_Bk7O-jcDU';
 // Create a new component. This component should produce some HTML
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'Peek-A-Boo'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Take this component's generated HTML and put it in the DOM
 ReactDom.render(<App />, document.querySelector('.container'));
