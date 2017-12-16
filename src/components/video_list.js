308ed7abb378f61801ce0c7beae1f1ab202d888a
kephin <wunshon76325@gmail.com>
2017-12-17 03:09:32 +0800

    add video_list_item component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // Remember to update all of our references from 'props' to 'this.props' when refactor component from a function-based component to a class-based component 
 const VideoList = (props) => {
+  const videoItems = props.videos.map(video => (
+    // Remember to add unique 'key' props when rendering an array or iterator
+    <VideoListItem key={video.etag} video={video}/>
+  ));
   return (
     // using 'className' in order to prevent naming conflict with JavaScript reserved keyword: 'class'
     <ul className='col-md-4 list-group'>
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
