bd1cfa165254785417c82a4b21bbc67f6a629190
kephin <wunshon76325@gmail.com>
2017-12-17 13:43:56 +0800

    add selectedVideo property to the App component state

--- src/index.js
+++ src/index.js
@@ -12,9 +12,15 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
     YTSearch({key: API_KEY, term: 'Peek-A-Boo'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
     });
   }
   
@@ -22,7 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
